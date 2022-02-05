babb05876c4827ecc6a707f9a49b5e37ffa03449
Duc Quang <ducquang03102000@gmail.com>
2022-02-05 23:37:01 +0700

    minor fix on loading

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,29 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
 import './Loading.scss';
-
-const pcVariants = {
-  hidden: {},
-  visible: {
-    transition: {
-      when: 'beforeChildren',
-      // this keyword specify when children should do
-      staggerChildren: 0.8,
-
-      // this keyword add after when to make the children animate with delay to each other
-      // ease: 'easeIn',
-    },
-  },
-};
-
-const childrenPCVariants = {
-  hidden: {
-    backgroundColor: '#243B55',
-  },
-  visible: {
-    backgroundColor: ['#243B55', '#ffffff', '#243B55'],
-  },
-};
+import { MobileLoading } from './mobile/MobileLoading';
+import { ComputerLoading } from './computer/ComputerLoading';
 
 export const Loading = () => {
   const [innerWidth, setInnerWidth] = useState(window.innerWidth);
@@ -45,40 +23,7 @@ export const Loading = () => {
 
   return (
     <div className="loading">
-      {renderView === 'PC' ? (
-        <>
-          <div className="flex items-center justify-center">
-            <motion.div
-              variants={pcVariants}
-              className="loading-box rounded-2xl"
-              initial="hidden"
-              animate="visible"
-            >
-              {[1, 2, 3, 4, 5].map((el) => {
-                return (
-                  <motion.div
-                    variants={childrenPCVariants}
-                    key={el}
-                    transition={{
-                      duration: 1,
-                      repeat: Infinity,
-                      repeatDelay: 0.8 * 4,
-                      ease: 'easeIn',
-                    }}
-                    className={`loading-pin ${el === 1 && 'rounded-l-xl'} ${
-                      el === 5 && 'rounded-r-xl'
-                    }`}
-                  ></motion.div>
-                );
-              })}
-            </motion.div>
-            <div className="circle"></div>
-          </div>
-          <p className="loading-text">Loading...</p>
-        </>
-      ) : (
-        <></>
-      )}
+      {renderView === 'PC' ? <ComputerLoading /> : <MobileLoading />}
     </div>
   );
 };
