da8fb334ae1e2a98c35018af1eab8612c447c720
Duc Quang <ducquang03102000@gmail.com>
2022-02-06 15:21:04 +0700

    feature/add-data-fetch-hook

--- src/common/hooks/DataFeching.ts
+++ src/common/hooks/DataFeching.ts
@@ -1,20 +1,31 @@
-import { useEffect, useState } from 'react';
-import { sampleTestApi } from '../../services/axiosConnection';
-import { ErrorTypes } from '../../services/types';
+import { useEffect, useRef, useState } from 'react';
 
-export function useDataFetching(endPointFetch: Function) {
-  const [loading, setLoading] = useState(false);
+export function useDataFetch(fetch: boolean, endPointFetch: Function) {
+  const [loading, setLoading] = useState(fetch);
   const [error, setError] = useState<string | undefined | object>(undefined);
+  const [result, setResult] = useState<any | undefined>(undefined);
+  const fetchData = useRef(() => {});
+  // fix eslint React Hook useEffect has a missing dependency: 'endPointFetch'. Either
+  // include it or remove the dependency array. If 'endPointFetch' changes too
+  // often, find the parent component that defines it and wrap that definition in useCallback
 
-  async function fetchData() {
+  fetchData.current = async () => {
     try {
-      setLoading(!loading);
-      const result = await endPointFetch();
+      const tmp = await endPointFetch();
+      setResult(tmp);
     } catch (error: any) {
       console.log(error);
       setError(error.message);
     } finally {
       setLoading(!loading);
     }
-  }
+  };
+
+  useEffect(() => {
+    if (loading) {
+      fetchData.current();
+    }
+  }, [loading]);
+
+  return [loading, setLoading, error, result];
 }
