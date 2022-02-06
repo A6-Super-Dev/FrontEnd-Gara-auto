e66be208aa619a88469d1d0d225fdc5315453837
Duc Quang <ducquang03102000@gmail.com>
2022-02-06 15:21:04 +0700

    feature/add fetch data hook

--- src/common/hooks/DataFeching.ts
+++ src/common/hooks/DataFeching.ts
@@ -1,31 +1,26 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function useDataFetch(fetch: boolean, endPointFetch: Function) {
   const [loading, setLoading] = useState(fetch);
   const [error, setError] = useState<string | undefined | object>(undefined);
   const [result, setResult] = useState<any | undefined>(undefined);
-  const fetchData = useRef(() => {});
-  // fix eslint React Hook useEffect has a missing dependency: 'endPointFetch'. Either
-  // include it or remove the dependency array. If 'endPointFetch' changes too
-  // often, find the parent component that defines it and wrap that definition in useCallback
-
-  fetchData.current = async () => {
-    try {
-      const tmp = await endPointFetch();
-      setResult(tmp);
-    } catch (error: any) {
-      console.log(error);
-      setError(error.message);
-    } finally {
-      setLoading(!loading);
-    }
-  };
 
   useEffect(() => {
-    if (loading) {
-      fetchData.current();
+    if (loading && endPointFetch) {
+      const fetchData = async () => {
+        try {
+          const tmp = await endPointFetch();
+          setResult(tmp);
+        } catch (error: any) {
+          console.log(error);
+          setError(error.message);
+        } finally {
+          setLoading(!loading);
+        }
+      };
+      fetchData();
     }
-  }, [loading]);
+  }, [loading, endPointFetch]);
 
   return [loading, setLoading, error, result];
 }
