7566139ac6b852b826a4639f166a935f9d4f5cb8
Duc Quang <ducquang03102000@gmail.com>
2022-03-30 16:57:31 +0700

    EW-0/chore: refator code with test library

--- src/common/config/routers/Router.tsx
+++ src/common/config/routers/Router.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { PasscodeRecover } from '../../../pages/admin/auth/passcodeRecover/PasscodeRecover';
-import { SignIn } from '../../../pages/admin/auth/signIn/SignIn';
 import { LogIn } from '../../../pages/client/auth/logIn/LogIn';
 import { PasswordRecover } from '../../../pages/client/auth/passwordRecover/PasswordRecover';
 import { SignUp } from '../../../pages/client/auth/signUp/SignUp';
@@ -10,30 +8,22 @@ import { routerPath } from '../../constants/routerPath';
 import { Route as Router } from '../interface/route';
 
 export const router: Router[] = [
-    {
-        path: routerPath.admin.auth.PASSCODE_RECOVER,
-        element: <PasscodeRecover />,
-    },
-    {
-        path: routerPath.admin.auth.SIGN_IN,
-        element: <SignIn />,
-    },
-    {
-        path: routerPath.client.auth.LOG_IN,
-        element: <LogIn />,
-    },
-    {
-        path: routerPath.client.auth.PASSWORD_RECOVER,
-        element: <PasswordRecover />,
-    },
-    {
-        path: routerPath.client.auth.SIGN_UP,
-        element: <SignUp />,
-    },
-    {
-        path: routerPath.client.common.HOME,
-        element: <Home />,
-    },
+  {
+    path: routerPath.client.auth.LOG_IN,
+    element: <LogIn />,
+  },
+  {
+    path: routerPath.client.auth.PASSWORD_RECOVER,
+    element: <PasswordRecover />,
+  },
+  {
+    path: routerPath.client.auth.SIGN_UP,
+    element: <SignUp />,
+  },
+  {
+    path: routerPath.client.common.HOME,
+    element: <Home />,
+  },
 ];
 
 export const allRouteName = router.map((el) => el.path);
--- src/pages/admin/auth/signIn/SignIn.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const SignIn: React.FunctionComponent = () => {
-  return <div>This is Sign In</div>;
-};
